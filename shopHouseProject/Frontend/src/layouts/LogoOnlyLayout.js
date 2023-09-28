c3fa33019b4b3b34c2fc3c5232b96be5fb9c0448
IT20254384 <it20254384@my.sliit.lk>
2023-09-28 13:00:57 +0530

    feat : Google oauth2 implementation sample

--- shopHouseProject/Frontend/src/layouts/LogoOnlyLayout.js
+++ shopHouseProject/Frontend/src/layouts/LogoOnlyLayout.js
@@ -1,39 +1,55 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from "react-router-dom";
 // material
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 // components
-import Logo from '../components/Logo';
-import { Button } from '@mui/material';
+import Logo from "../components/Logo";
+import { Button } from "@mui/material";
 
 // ----------------------------------------------------------------------
 
-const HeaderStyle = styled('header')(({ theme }) => ({
+const HeaderStyle = styled("header")(({ theme }) => ({
   top: 0,
   left: 0,
   lineHeight: 0,
-  width: '100%',
-  position: 'absolute',
+  width: "100%",
+  position: "absolute",
   padding: theme.spacing(3, 3, 0),
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     padding: theme.spacing(5, 5, 0),
   },
 }));
 
+const googleAuth = () => {
+  window.open(`http://localhost:5000/auth/google/callback`, "_self");
+};
+
 // ----------------------------------------------------------------------
 
 export default function LogoOnlyLayout() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
   console.log(window.location.pathname);
   return (
     <>
-      <HeaderStyle>
-        {/* <Logo /> */}
-      </HeaderStyle>
-      {window.location.pathname == "/" &&
+      <HeaderStyle>{/* <Logo /> */}</HeaderStyle>
+      {window.location.pathname == "/" && (
         <>
-          <Button onClick={() => { navigate(`/buyer/home`) }}>Home Page</Button>
+          <Button
+            onClick={() => {
+              navigate(`/buyer/home`);
+            }}
+          >
+            Home Page
+          </Button>
+
+          <Button
+            onClick={() => {
+              googleAuth();
+            }}
+          >
+            Sign Up
+          </Button>
         </>
-      }
+      )}
       <Outlet />
     </>
   );
